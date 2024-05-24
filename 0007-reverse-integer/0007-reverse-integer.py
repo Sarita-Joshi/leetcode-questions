1d909e40b06427d510926bfe1a91c695aab0a3e3
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-05-24 14:01:29 -0700

    Time: 15 ms (62.20%), Space: 11.5 MB (94.25%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -7,24 +7,19 @@ class Solution(object):
         change_sign = False
         if x < 0:
             change_sign = True
-            x = abs(x)
+            x = -1*x
         
-        digits = []
+        res = 0
         while x !=0:
-            digits.append(x%10)
+            res = res * 10 + x%10
             x = x//10
             
-        res = 0
-        for n in digits:
-            res = res * 10 + n
         
         if res > 2**31-1:
             return 0
         
         if change_sign:
             res = res * -1
-        
-        
         
         return res
         
