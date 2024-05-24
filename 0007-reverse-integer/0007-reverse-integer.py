b880ef22f8d8d4f477353dea7977ea5e89f2737e
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-05-24 14:05:35 -0700

    Time: 44 ms (9.80%), Space: 16.6 MB (68.47%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+class Solution:
+    def reverse(self, x: int) -> int:
         change_sign = False
         if x < 0:
             change_sign = True
             x = -1*x
         
-        res = 0
-        while x !=0:
-            res = res * 10 + x%10
-            x = x//10
+        res = int(str(x)[::-1])
+        # res = 0
+        # while x !=0:
+        #     res = res * 10 + x%10
+        #     x = x//10
             
         
         if res > 2**31-1:
