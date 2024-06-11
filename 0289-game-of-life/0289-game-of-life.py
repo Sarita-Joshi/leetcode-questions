6d5c6978f097f139b139f0070399a81e593a0382
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-10 21:32:55 -0700

    Time: 41 ms (24.86%), Space: 16.5 MB (47.75%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,37 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m, n = len(board), len(board[0])
+        change_state = []
+        inc = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+        def get_neighbors(r,c):
+            count = 0
+            for i,j in inc:
+                if 0 <= r+i < m and 0 <= c+j < n and board[r+i][c+j] == 1:
+                    count += 1
+            return count
+            
+
+        for i in range(m):
+            for j in range(n):
+                neighbors = get_neighbors(i,j)
+                if neighbors<2 and board[i][j]==1:
+                    change_state.append((i,j,0))
+                elif neighbors>3 and board[i][j]==1:
+                    change_state.append((i,j,0))
+                elif neighbors==3 and board[i][j]==0:
+                    change_state.append((i,j,1))
+                    
+        for i,j,state in change_state:
+            board[i][j] = state
+            
+            
+                
+                
+                
+                
+                
+        
+        
