37ac8327b0d2478ebe3497f5f55e27ecfc1fb367
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-11 14:22:04 -0700

    Time: 18 ms (99.85%), Space: 16.4 MB (98.78%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,6 +1,31 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        # s = s.split()
+        # pairings = set(zip(pattern, s))
+        # # print(pairings)
+        # return len(pairings)==len(set(pattern))==len(set(s)) and len(pattern) == len(s)
+        
+        
         s = s.split()
-        pairings = set(zip(pattern, s))
-        # print(pairings)
-        return len(pairings)==len(set(pattern))==len(set(s)) and len(pattern) == len(s)
+        if len(pattern) != len(s):
+            return False
+        
+        su = {}
+        pu = {}
+        
+        for i in range(len(s)):
+            if s[i] not in su:
+                su[s[i]] = pattern[i]
+            
+            if pattern[i] not in pu:
+                pu[pattern[i]] = s[i]
+            
+            if su[s[i]]!=pattern[i] or pu[pattern[i]] != s[i]:
+                return False
+            
+        return True
+                
+        
+        
+        
+        
