d76b1eec89fab054c2e921e85703e0b507814042
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-11 14:33:11 -0700

    Time: 88 ms (64.49%), Space: 20.5 MB (43.51%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        if len(strs)==1:
+            return [strs]
+        
+        res = defaultdict(list)
+        
+        for s in strs:
+            res[tuple(sorted(s))].append(s)
+        
+        return [v for v in res.values()]
+            
+            
