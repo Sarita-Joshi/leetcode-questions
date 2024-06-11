1343dbace15e77bf862c1507e6e646ecfce685cd
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-10 21:52:27 -0700

    Time: 44 ms (44.50%), Space: 16.7 MB (51.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,19 +2,15 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         su = {}
         tu = {}
-
-        scount = 0
-        tcount = 0
         for i in range(len(s)):
             if s[i] not in su:
-                su[s[i]] = scount
-                scount += 1
+                su[s[i]] = t[i]
+                
 
             if t[i] not in tu:
-                tu[t[i]] = tcount
-                tcount += 1
+                tu[t[i]] = s[i]
                 
-            if su[s[i]] != tu[t[i]]:
+            if su[s[i]] != t[i] or tu[t[i]] != s[i]:
                 return False
                             
         
