495c47cc6752da4ae35df38ba29676d0e186c2dd
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-10 20:26:19 -0700

    Time: 95 ms (36.66%), Space: 16.5 MB (88.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,31 +1,25 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-#         rows = collections.defaultdict(list)
-#         cols = collections.defaultdict(list)
-#         boxes = collections.defaultdict(list)
+        rows = collections.defaultdict(list)
+        cols = collections.defaultdict(list)
+        boxes = collections.defaultdict(list)
         
     
-#         for i in range(9):
-#             for j in range(9):
-#                 cell = board[i][j]
-#                 if cell == ".":
-#                     continue
-                    
-#                 if cell in rows[i] or cell in cols[j] or cell in boxes[(i//3,j//3)]:
-#                     return False
-                
-#                 rows[i].append(cell) 
-#                 cols[j].append(cell)
-#                 boxes[(i//3,j//3)].append(cell)
-                
-#         return True
-
-        res = []
         for i in range(9):
             for j in range(9):
-                element = board[i][j]
-                if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
+                cell = board[i][j]
+                if cell == ".":
+                    continue
+                    
+                if cell in rows[i] or cell in cols[j] or cell in boxes[(i//3,j//3)]:
+                    return False
+                
+                rows[i].append(cell) 
+                cols[j].append(cell)
+                boxes[(i//3,j//3)].append(cell)
+                
+        return True
+
+
                 
