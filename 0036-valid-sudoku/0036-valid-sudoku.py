eb4e1e5cb8be2d2956777a3b56932ef0909ad98d
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-10 20:27:22 -0700

    Time: 98 ms (21.96%), Space: 16.5 MB (88.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,7 +4,6 @@ class Solution:
         rows = collections.defaultdict(list)
         cols = collections.defaultdict(list)
         boxes = collections.defaultdict(list)
-        
     
         for i in range(9):
             for j in range(9):
