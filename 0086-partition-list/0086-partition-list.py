6c2b68bc866aa0674e91248006b28f7f9398c5f4
Sarita Joshi <43993824+Sarita-Joshi@users.noreply.github.com>
2024-06-15 18:08:09 -0700

    Time: 36 ms (64.82%), Space: 16.6 MB (48.53%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -4,34 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+    def partition(self, curr: Optional[ListNode], x: int) -> Optional[ListNode]:
         
-        start = ListNode()
-        end = start
-        parts = start
-        curr = head
-        
+        big = ListNode(0)
+        small = ListNode(0)
+        bigcurr = big
+        smallcurr = small
         while curr:
-            if end==start:
-                end.next = ListNode(curr.val)
-                end = end.next
-                
-                if curr.val < x:
-                    parts = parts.next
-                
-            elif curr.val >= x:
-                end.next = ListNode(curr.val)
-                end = end.next
+            
+            if curr.val >= x:
+                bigcurr.next = ListNode(curr.val)
+                bigcurr = bigcurr.next
                 
                 if curr.val < x:
                     parts = parts.next
             else:
-                parts.next = ListNode(curr.val, parts.next)
-                if parts==end:
-                    end = end.next
-                parts = parts.next
-                
+                smallcurr.next = ListNode(curr.val)
+                smallcurr = smallcurr.next
             
             curr = curr.next
             
-        return start.next
+        smallcurr.next = big.next
+            
+        return small.next
